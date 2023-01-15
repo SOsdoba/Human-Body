940797e6ca9af09bd6c7628ee6f29749b073c917
sosdoba <saraosdoba1@gmail.com>
2023-01-14 21:54:38 -0500

    code review see comments in files

--- 2 sample data/2 sample data.sql
+++ 2 sample data/2 sample data.sql
@@ -1,8 +1,9 @@
+--SO please populate sample data with planets to match your new data schema
 insert body(bodypart, num)
 select 'eyes', 2
 union select 'ears', 2
 union select 'nose', 1
-union select 'muth', 2
+union select 'muth', 2 --SO was this change unintentional, I don't think you need to change the spelling of this word for this schema
 union select 'teeth', 32
 union select 'toungue', 1
 
--- 2 sample data/planets.sql
+++ 2 sample data/planets.sql
@@ -1,3 +1,4 @@
+--SO this probably belongs in sample data
 earth
 mars
 jupiter
