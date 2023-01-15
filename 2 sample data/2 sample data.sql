e7aaad8549ad9b85f759be6d706a9781f59ee947
sosdoba <saraosdoba1@gmail.com>
2023-01-14 21:31:11 -0500

    1st commit for trial branch

--- 2 sample data/2 sample data.sql
+++ 2 sample data/2 sample data.sql
@@ -5,3 +5,7 @@ union select 'nose', 1
 union select 'mouth', 2
 union select 'teeth', 32
 union select 'toungue', 1
+
+--added hair... how should we store the hair part in the data
+insert body(bodypart, num)
+select 'hair', 4000
